8a681f8188b3d29bc7505c8786a977a159701c50
mmwwwhahaha <angie@goldkrush.com>
2020-07-20 11:08:35 -0500

    Moved extensions to Extensions file

--- Sources/Colt/Extensions.swift
+++ Sources/Colt/Extensions.swift
@@ -5,5 +5,39 @@
 //  Created by Angie Sanders on 6/17/20.
 //
 
-//import Foundation
+import Foundation
 //Foundation' inconsistently imported as implementation-only
+
+//TODO: Move to Extensions file
+
+extension String {
+    func appendLineToURL(fileURL: URL) throws {
+        try (self + "\n").appendToURL(fileURL: fileURL)
+    }
+
+    func appendToURL(fileURL: URL) throws {
+        let data = self.data(using: String.Encoding.utf8)!
+        try data.append(fileURL: fileURL)
+    }
+    
+    func stringByAddingPercentEncoding() -> String? {
+      let unreserved = "-._~/?"
+      let allowed = NSMutableCharacterSet.alphanumeric()
+      allowed.addCharacters(in: unreserved)
+      return addingPercentEncoding(withAllowedCharacters: allowed as CharacterSet)
+    }
+}
+
+extension Data {
+    func append(fileURL: URL) throws {
+        if let fileHandle = FileHandle(forWritingAtPath: fileURL.path) {
+            defer {
+                fileHandle.closeFile()
+            }
+            fileHandle.seekToEndOfFile()
+            fileHandle.write(self)
+        } else {
+            try write(to: fileURL, options: .atomic)
+        }
+    }
+}
--- Sources/Colt/main.swift
+++ Sources/Colt/main.swift
@@ -191,53 +191,9 @@ func createNewStringsFile(folderUrl: URL) {
     }
 }
 
-//
-//func copyStringsFile(to: URL, source: URL) {
-//    do {
-//        try localFileManager.copyItem(atPath: source.absoluteString, toPath: to.absoluteString)
-//    } catch {
-//        print("copy failed")
-//    }
-//}
-
 func showError(_ error: String) {
     print(error)
     exit(EXIT_FAILURE)
 }
 
 Translate.main()
-
-//TODO: Move to Extensions file
-
-extension String {
-    func appendLineToURL(fileURL: URL) throws {
-        try (self + "\n").appendToURL(fileURL: fileURL)
-    }
-
-    func appendToURL(fileURL: URL) throws {
-        let data = self.data(using: String.Encoding.utf8)!
-        try data.append(fileURL: fileURL)
-    }
-    
-    func stringByAddingPercentEncoding() -> String? {
-      let unreserved = "-._~/?"
-      let allowed = NSMutableCharacterSet.alphanumeric()
-      allowed.addCharacters(in: unreserved)
-      return addingPercentEncoding(withAllowedCharacters: allowed as CharacterSet)
-    }
-}
-
-extension Data {
-    func append(fileURL: URL) throws {
-        if let fileHandle = FileHandle(forWritingAtPath: fileURL.path) {
-            defer {
-                fileHandle.closeFile()
-            }
-            fileHandle.seekToEndOfFile()
-            fileHandle.write(self)
-        } else {
-            try write(to: fileURL, options: .atomic)
-        }
-    }
-}
-
